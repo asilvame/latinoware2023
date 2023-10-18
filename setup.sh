19afc16d69f141595f5139da311e7e4e0a6f9119
André da Silva Mesquita <86494923+asilvame@users.noreply.github.com>
2023-10-18 15:18:12 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,8 @@ sudo usermod -aG dialout latinoware
 sudo yum install screen
 cd /home/latinoware
 mkdir espmpy
-cd espmy
+cd espmpy
 python -m venv mpy 
 source /home/latinoware/espmpy/mpy/activate
 pip install jupyter lab
+pip install adafruit_ampy
