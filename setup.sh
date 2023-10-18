78639a7dc4e72014ec8a9b4c36c9f504db4e6100
André da Silva Mesquita <86494923+asilvame@users.noreply.github.com>
2023-10-18 15:08:31 -0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+sudo usermod -aG dialout latinoware
+sudo yum install screen
+cd /home/latinoware
+mkdir espmpy
+cd espmy
+python -m venv mpy 
+source /home/latinoware/espmpy/mpy/activate
+pip install jupyter lab
