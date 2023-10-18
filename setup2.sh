6d29e9fde3a9515c11afdd9b67978564e27b6669
André da Silva Mesquita <86494923+asilvame@users.noreply.github.com>
2023-10-18 16:05:29 -0300

    Create setup2.sh

---
+++ setup2.sh
@@ -0,0 +1,18 @@
+#sudo usermod -aG dialout latinoware
+sudo usermod -aG dialout latinoware2023
+sudo yum -y install screen
+sudo yum -y install podman-compose
+##Oficina Franzininho C0
+sudo yum -y install openjfx
+podman pull docker.io/library/nginx
+podman pull docker.io/library/nginx
+cd 
+mkdir espmpy
+cd espmpy
+git clone https://github.com/micropython/webrepl.git
+python -m venv mpy 
+source ~/espmpy/mpy/bin/activate
+pip install --upgrade pip
+pip install jupyter lab
+pip install adafruit_ampy
+sudo shutdown -r now 
