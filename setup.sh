4c7b036dfc4c6d59b067e5a0d4fbd020e2458e50
André da Silva Mesquita <86494923+asilvame@users.noreply.github.com>
2023-10-18 15:34:37 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,14 @@
 sudo usermod -aG dialout latinoware
 sudo yum install screen
+sudo yum install podman-compose
+podman pull docker.io/library/nginx
 cd /home/latinoware
 mkdir espmpy
 cd espmpy
+git clone https://github.com/micropython/webrepl.git
 python -m venv mpy 
 source /home/latinoware/espmpy/mpy/bin/activate
+pip install --upgrade pip
 pip install jupyter lab
 pip install adafruit_ampy
+sudo shutdown -r now 
